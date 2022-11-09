a6a6951bd21eb7c870c16fd11a11b92b80a0bba8
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-11-09 15:30:23 -0300

    commit aula 7

---
+++ Javascript-Curso-em-Vídeo/aula07/index.js
@@ -0,0 +1,35 @@
+console.log(5+2)
+console.log(9%2)
+console.log(9/2)
+console.log("")
+
+console.log(5 + 3 / 2)
+console.log((5+3)/2)
+console.log("")
+
+var f = 3 % 2 + 4 / 2
+console.log(f)
+console.log("")
+
+var num = 8
+console.log(num)
+num+= 2
+console.log(num)
+num%=2
+console.log(num)
+num = 3
+console.log(num)
+num **= 2
+console.log(num)
+console.log("")
+
+var n = 10
+console.log(n)
+n++
+console.log(n)
+n--
+console.log(n)
+++n
+console.log(n)
+--n
+console.log(n)
