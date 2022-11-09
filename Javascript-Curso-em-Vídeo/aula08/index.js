c9bb298ed8354a1ae0d0e4a1b3b34e17fe53e136
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-11-09 15:54:20 -0300

    commit aula 8

---
+++ Javascript-Curso-em-Vídeo/aula08/index.js
@@ -0,0 +1,47 @@
+console.log(5>2)
+console.log(8<4)
+console.log("")
+
+var a = 8
+var b = 15
+console.log(a>b)
+console.log(a<b)
+console.log(a<= b - 10)
+console.log(a==b)
+console.log(a!=b)
+console.log('')
+
+console.log('5'==5)
+console.log('5'===5)
+console.log('')
+
+var a = 5
+var b = 8
+console.log(true && false)
+console.log(true && true)
+console.log(true || false)
+console.log(true || true)
+console.log('')
+
+console.log(a>b && b%2 == 0)
+console.log(a <=b || b/ 2 ==2)
+console.log("")
+
+var media = 5.5
+console.log(media)
+console.log(media>7?'aprovado':'reprovado')
+media+=3
+console.log(media)
+console.log(media>7?'aprovado':'reprovado')
+console.log('')
+
+var x = 8
+var res = x%2 == 0? 5:9
+console.log(x)
+console.log(res)
+console.log('')
+
+var idade = 19
+var r = idade>=18?'de maior':'de menor'
+console.log(idade)
+console.log(r)
