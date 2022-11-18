48c825c1ae409d13126030750b2c592ed07dd1e1
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-11-18 16:27:09 -0300

    commit aula11

---
+++ Javascript-Curso-em-Vídeo/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel>60){
+    console.log('você ultrapassou a velocidade permitida. MULTADO!!')
+}
+
+console.log('dirija sempre usando cinto de segurança')
---
+++ Javascript-Curso-em-Vídeo/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+
+console.log(`vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('vc é brasileiro')
+}else{
+    console.log('vc é estrangeiro')
+}
